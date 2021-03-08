32c5280193470cc141ba087d61616990890c5528
nicksonlangat <nicksonlangat95@gmail.com>
2021-03-08 18:08:41 +0300

    b

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +7,59 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private camera: Camera) {}
+
+  private optionsCamera: CameraOptions = {
+    quality: 100,
+    targetWidth: 600,
+    sourceType: this.camera.PictureSourceType.CAMERA,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  }
+
+  private optionsGallery: CameraOptions = {
+    quality: 100,
+    sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  }
+  openCamera(){
+    this.camera.getPicture(this.optionsCamera).then((imageData) => {
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      console.log(base64Image)
+     }, (err) => {
+      // Handle error
+      console.log(err)
+     })
+  }
+  
+  openGallery(){
+    this.camera.getPicture(this.optionsGallery).then((imageData) => {
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+      console.log(base64Image)
+     }, (err) => {
+      // Handle error
+      console.log(err)
+     })
+  }
+
+  takePhoto(sourceType:number) {
+    const options: CameraOptions = {
+      quality: 50,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      correctOrientation: true,
+      sourceType:sourceType,
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+    });
+  }
 
 }
